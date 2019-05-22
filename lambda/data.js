64be2cb4716f636474654dbaa3bb4a832a7315e0
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-05-22 15:39:17 +0000

    updated lambda/data.js.

---
+++ lambda/data.js
@@ -0,0 +1,36 @@
+var questions = [
+    "Do you like hot spring?",
+    "Do you want to drive?",
+    "Is air quality important to you?",
+    "Do you want to have street food?",
+    "Do you want to go to America?",
+    "Do you like museum?",
+    "Do you want to have Asian food?",
+    "Do you like scenic train ride?",
+    "Do you want to visit top university?",
+    "Do you wish to encounter movie stars?",
+    "Do you want to explore languages other than Chinese and English?"
+];
+
+var destinations = [
+    "Budapest",
+    "Mexico City",
+    "Hakone",
+    "Taipei",
+    "Heidelberg",
+    "Los Angeles"
+]
+
+var question_destination_match = [
+    ["Budapest", "Hakone", "Taipei"],
+    ["Mexico City", "Hakone", "Heidelberg", "Los Angeles"],
+    ["Budapest", "Hakone", "Heidelberg"],
+    ["Mexico City", "Taipei"],
+    ["Mexico City", "Los Angeles"],
+    ["Mexico City", "Taipei", "Los Angeles"],
+    ["Hakone", "Taipei"],
+    ["Hakone"],
+    ["Taipei", "Heidelberg", "Los Angeles"],
+    ["Taipei", "Los Angeles"],
+    ["Heidelberg", "Los Angeles"]
+];
