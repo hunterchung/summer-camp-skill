f0dfeccbef18461a6d2f0c720a1a2ee066619866
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-05-22 15:57:58 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,7 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
-let data = require('./data')
+const data = require('./data')
 
 const QuizIntentHandler = {
     canHandle(handlerInput) {
